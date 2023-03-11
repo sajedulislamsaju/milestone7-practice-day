9c5d81d506508f8a9342541c5b38dcde4c05834e
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-03-11 17:41:40 +0600

    this is the  secend commit

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,15 @@
-console.log('This is the loging console log')
+// ccc
+let num = 0;
+
+const interval = setInterval(()=>{
+    // num++;
+    console.log(++num);
+
+    if (num === 10) {
+        clearInterval (interval);
+    }
+},1000)
+// console.log(6)
+// console.log(8)
+// console.log(1)
+// console.log(9)
